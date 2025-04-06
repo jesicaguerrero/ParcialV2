4f70c9ef0f97807eac7524d3ef6034f696577ff1
jessica <jeguebla00@gmail.com>
2025-04-06 15:50:33 -0500

    first commit - web server

---
+++ src/app.js
@@ -0,0 +1,8 @@
+
+import express from "express";
+
+const app = express();
+
+app.set("port",5000)
+
+export default app; 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import app from "./app.js";
+const main =() => {
+    app.listen(app.get("port"));
+    console.log(`The company's server web is running on port ${app.get("port")}`);
+}
+main();
